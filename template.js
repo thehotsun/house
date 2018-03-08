23e12aeb072636a2cc0d91d96fc6763c4dd448ca
thehotsun <3239601320>
2018-03-08 16:17:23 +0800

    完善

--- template.js
+++ template.js
@@ -1,7 +1,10 @@
 // 实现简易模板
-function temp (id, str) {
-  var arr = []
+var str = document.getElementById('id').innerHTML
+var arr = []
+function temp (id) {
   str = 'arr.push("' + str.replace(/<%=(.*)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '")'
-  eval(str)
+  var fn = new  Function(str)
+  fn()
+  // Function 这种情形定义的函数，上下文只在全局作用域中定义，也就是只能读取自身作用域以及全局作用域的函数
   document.getElementById(id).innerHTML = arr.join('')
 }
