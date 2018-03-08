f1b60c2f0f1088a9a2299a086b90fe03e85c6b98
thehotsun <3239601320>
2018-03-08 16:04:44 +0800

    实现了简易解析ejs模板的函数

--- template.js
+++ template.js
@@ -2,8 +2,6 @@
 function temp (id, str) {
   var arr = []
   str = 'arr.push("' + str.replace(/<%=(.*)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '")'
-  console.log(str);
   eval(str)
-  console.log(arr);
   document.getElementById(id).innerHTML = arr.join('')
 }
