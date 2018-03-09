4b1c3c6f4df6d082893ab91b94bdd5640d661861
thehotsun <3239601320>
2018-03-09 10:58:54 +0800

    完善

--- template.js
+++ template.js
@@ -13,11 +13,9 @@
 
 var str = document.getElementById('ww').innerHTML
 function temp (id) {
-  var arr = []
   var data = [{url: 'https://www.baidu.com', name : 'xz'}, {url: 'https://www.baidu.com', name : 'xz'}, {url:'https://www.baidu.com', name : 'xz'}]
-  str = 'arr.push("' + str.replace(/<%=(.*)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '")'
-  var fn = new  Function('users','arr', str)
-  fn(data, arr)
+  str = 'var arr= []; arr.push("' + str.replace(/<%=(.*?)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '");return arr.join("")'
+  var fn = new  Function('users', str)
   // Function 这种情形定义的函数，上下文只在全局作用域中定义，也就是只能读取自身作用域以及全局作用域的函数
-  document.getElementById(id).innerHTML = arr.join('')
+  document.getElementById(id).innerHTML = fn(data)
 }
