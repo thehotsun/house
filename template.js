3e8130f5055525a7008ef80a930fe9a4b39d750a
thehotsun <3239601320>
2018-03-12 09:57:31 +0800

    完善

--- template.js
+++ template.js
@@ -14,7 +14,7 @@
 var str = document.getElementById('ww').innerHTML
 function temp (id) {
   var data = [{url: 'https://www.baidu.com', name : 'xz'}, {url: 'https://www.baidu.com', name : 'xz'}, {url:'https://www.baidu.com', name : 'xz'}]
-  str = 'var arr= []; arr.push("' + str.replace(/<%=(.*?)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '");return arr.join("")'
+  str = 'var arr= []; arr.push("' + str.replace(/<%=([/s/S]+?)%>/gm, function(_, $1) {return '");arr.push(' + $1 + ');' + 'arr.push("'}).replace(/(%>$)/g, '').replace(/<%/gm, '");').replace(/%>/g, 'arr.push("').replace(/[\r\t\n]/g, '') + '");return arr.join("")'
   var fn = new  Function('users', str)
   // Function 这种情形定义的函数，上下文只在全局作用域中定义，也就是只能读取自身作用域以及全局作用域的函数
   document.getElementById(id).innerHTML = fn(data)
